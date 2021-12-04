39fb4cd80a126efe575f2bba6f3c9f3fcdbe3a9e
MasterKenth <MasterKenth@users.noreply.github.com>
2021-12-04 21:43:56 +0100

    :wrench:  add initial config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  ignorePatterns: ["public/*"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+  },
+  plugins: ["jest"],
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "prettier",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      plugins: ["@typescript-eslint"],
+      parser: "@typescript-eslint/parser",
+      extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+    },
+  ],
+};
